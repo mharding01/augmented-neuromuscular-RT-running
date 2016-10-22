9b1bf6a3f9d9301334436fdf13738e91519e40e5
Matthew <matthew.harding@epfl.ch>
2016-10-22 08:35:12 -0400

    Added 'save' folder for opti results * Also added 'efficiency' scripts for navigating project faster

---
+++ scripts/eff.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Define variables and aliases to ease navigating the code in this projcet
+
+TOP=~/BIOROB/coman_matthew
+WORK=$TOP/workR
+USERFILES=$TOP/userFiles
+USERFCTR=$TOP/userfctR
+BUILD=$WORK/build
+SRC=$WORK/src
+U_CTRL=$USERFILES/ctrl
+
