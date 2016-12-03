10b8a39b4a02586348ab12d4f99819a1164474fa
Matthew <matthew.harding@epfl.ch>
2016-12-03 05:38:15 -0500

    Changed Matsuoka osc's so error is calculated from first step, prepared for another 1.6m/s opti

--- userFiles/ctrl/body/oscillators/specific_osc/MatsuokaSixN.cc
+++ userFiles/ctrl/body/oscillators/specific_osc/MatsuokaSixN.cc
@@ -1,7 +1,7 @@
-
+#include <stdio.h>
 #include "MatsuokaSixN.hh"
 
-#define NB_STEPS_OSC_ERROS 5 ///< number of steps before computing the oscillators prediction errors [-]
+#define NB_STEPS_OSC_ERROS 0 ///< number of steps before computing the oscillators prediction errors [-]
 #define OSC_EXCITATION 1.0   ///< excitation of the oscillators
 
 #define MIN_THETA_REF 0.005
@@ -146,12 +146,12 @@ void MatsuokaSixN::Matsuoka_six_neurons()
 	vd[4] = tau_B_inv * ( -v[4] + pos(x[4]) );
 	vd[5] = tau_C_inv * ( -v[5] + pos(x[5]) ); 
     
-    //set_plot(x[0], "x1");
-    //set_plot(x[1], "x2");
-    //set_plot(x[3] , "x4");
-    //set_plot(x[4] , "x5");
-    //set_plot(x[2] , "x3");
-    //set_plot(x[5] , "x6");
+    set_plot(x[0], "x1");
+    set_plot(x[1], "x2");
+    set_plot(x[3] , "x4");
+    set_plot(x[4] , "x5");
+    set_plot(x[2] , "x3");
+    set_plot(x[5] , "x6");
 
     // Plot fatigue
     //set_plot(v[0], "V1");
