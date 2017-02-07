021061fad5f4138fd7396148cffab5668d165562
Matthew Harding <matthew.harding@epfl.ch>
2017-02-07 17:50:01 -0500

    New param fitting bounds for new opti, informed by fixed run_params.py

--- opti/config/OptiParams.cc
+++ opti/config/OptiParams.cc
@@ -36,39 +36,39 @@ void OPTI_NAME::set_opti()
     g_osc->set_eta_E({3.64; 3.72 });  
 
     // Fitting parameters for improved vel tracking
-    g_osc->set_P_theta_trunk({0.0; .20});
-    g_osc->set_P_theta_hip({0.0; 1.0});
-    g_osc->set_P_k_theta({0.25; 15.0});
+    g_osc->set_P_theta_trunk({-0.05; .15});
+    g_osc->set_P_theta_hip({0.0; 0.3});
+    g_osc->set_P_k_theta({0.0; 12.0});
     
     // Linear
-    g_osc->set_P_k_HFLrun1({0.0; 6.1});
-    g_osc->set_p_k_HFLrun1({-2.0; 2.0});
+    g_osc->set_P_k_HFLrun1({0.2; 7.1});
+    g_osc->set_p_k_HFLrun1({-5.0; 1.0});
 
-    g_osc->set_P_G_SOL_TA({0.4; 7.0});
-    g_osc->set_p_G_SOL_TA({-2.0; 2.0});
+    g_osc->set_P_G_SOL_TA({0.4; 8.0});
+    g_osc->set_p_G_SOL_TA({0.0; 15.0});
 
-    g_osc->set_P_G_GAS({0.0; 20.0});
-    g_osc->set_p_G_GAS({-2.0; 3.0});
+    g_osc->set_P_G_GAS({0.0; 11.0});
+    g_osc->set_p_G_GAS({-20.0; 0.0});
 
-    g_osc->set_P_G_VAS({0.82; 5.0});
-    g_osc->set_p_G_VAS({-2.0; 2.0});
+    g_osc->set_P_G_VAS({0.42; 5.0});
+    g_osc->set_p_G_VAS({-2.0; 5.0});
     
     // Quadratic
     g_osc->set_P_tau({0.010; .10});
-    g_osc->set_p_tau({-2.0; 2.0});
-    g_osc->set_p2_tau({-2.0; 2.0});
+    g_osc->set_p_tau({-1.0; 0.0});
+    g_osc->set_p2_tau({-1.0;0.0});
     
     g_osc->set_P_k_HFLrun2({1.0;10.0});
-    g_osc->set_p_k_HFLrun2({-3.0;3.0});
-    g_osc->set_p2_k_HFLrun2({-10.0;10.0});
+    g_osc->set_p_k_HFLrun2({1.0;40.0});
+    g_osc->set_p2_k_HFLrun2({0.0;110.0});
     
     g_osc->set_P_k_HAMrun({0.3;7.0});
-    g_osc->set_p_k_HAMrun({-3.0;5.0});
-    g_osc->set_p2_k_HAMrun({-10.0;10.0});
+    g_osc->set_p_k_HAMrun({0.2;10.0});
+    g_osc->set_p2_k_HAMrun({0.3;45.0});
 
-    g_osc->set_P_G_SOL({0.97;5.0});
-    g_osc->set_p_G_SOL({-3.0;3.0});
-    g_osc->set_p2_G_SOL({-10.0;10.0});
+    g_osc->set_P_G_SOL({0.4;5.0});
+    g_osc->set_p_G_SOL({0.4;8.0});
+    g_osc->set_p2_G_SOL({5.0;19.0});
 
 	// Wang stimulations
     // TODO: added cpg_ctrl_thresh_t parameter for optimization
