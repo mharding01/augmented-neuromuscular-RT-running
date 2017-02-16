12fa82963eb00ad6c33668e9df73ac048ab3ba2b
Matthew Harding <matthew.harding@epfl.ch>
2017-02-16 00:14:35 -0500

    Updated opti speed_fitness range to be 1.1-1.9m/s

--- workR/src/simu_run.cc
+++ workR/src/simu_run.cc
@@ -198,8 +198,8 @@ double speed_range_fitness(OptiClass *optiClass)
 	std::vector<double> v_ref_tab;
 
 	// variables initialization
-	nb_runs = 9;
-	init_v  = 1.3;
+	nb_runs = 17; 	// 1.1 - 1.9m/s
+	init_v  = 1.1;
 	delta_v = 0.05;
 
 	fitness = 0.0;
