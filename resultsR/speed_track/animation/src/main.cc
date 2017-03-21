8ffe310680a546fe6760f7ab8c81778d47b89919
Nicolas Van der Noot <nicolas.vandernoot@uclouvain.be>
2017-03-21 10:15:53 +0100

    shadows working

--- resultsR/speed_track/animation/src/main.cc
+++ resultsR/speed_track/animation/src/main.cc
@@ -6,6 +6,7 @@
 #include "MbsWorld3D.hh"
 #include "cmake_config.h"
 #include "AnimReader.hh"
+#include "shader_multisample.hh"
 #include <iostream>
 
 #define NB_JOINTS_COMAN 31 ///< number of joints (COMAN)
@@ -17,10 +18,14 @@ int main(int argc, char **argv)
 	// variables declaration
 	double tsim;
 	double *joint_vec;
+	int shader_flag, multi_samp;
 	OpenGLMbs::AnimReader *reader;
 
+    // get the shader flag and the mutlisampling option from .mbs file
+    OpenGLMbs::shader_multisample(PROJECT_SOURCE_DIR"/dataR/coman_spring_toe_short_feet.mbs", shader_flag, multi_samp);
+
 	// main window for 3D world
-	OpenGLMbs::MbsWorld3D world_3d(1024, 768);
+	OpenGLMbs::MbsWorld3D world_3d(1024, 768, shader_flag, multi_samp);
 
 	// load mbs files
 	world_3d.AddMbs(PROJECT_SOURCE_DIR"/dataR/coman_spring_toe_short_feet.mbs");
